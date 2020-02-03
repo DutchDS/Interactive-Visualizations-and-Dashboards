32860ba0659984f95934767e63613f5d0f987e5f
DutchDS <dutchdatasmarts@gmail.com>
2020-02-03 07:10:12 -0600

    All done except for commenting and cleaning up code

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -72,6 +72,8 @@ function optionChanged() {
     console.log(outputSampleData);
 
     loadSampleMetadata(outputMetaData);
+    createBubble(outputSampleData);
+    createGauge(outputMetaData)
     createBar(outputSampleData);
     }
 
@@ -110,8 +112,7 @@ function createBar(data) {
     indices = indices.slice(0,10).reverse();
     console.log(indices);
 
-
-    data.otu_ids = indices.map(i => ("UFO " + data.otu_ids[i]));
+    data.otu_ids = indices.map(i => ("UFO " + data.otu_ids[i] + " "));
     data.sample_values = indices.map(i => data.sample_values[i]);
     data.otu_labels = indices.map(i => data.otu_labels[i]);
 
@@ -126,26 +127,133 @@ function createBar(data) {
     text: data.otu_labels,
     name: "OTU",
     type: "bar",
-    orientation: "h"
-    };
+    orientation: "h",
+    marker: {color: "#023b55"} 
+   };
 
     // data
     var bar_data = [trace1];
 
     // Apply the group bar mode to the layout
     var layout = {
-    title: "OTU Results for selected ID",
+    title: "Test Subject ID: " + data.id,
+    xaxis: {title:"Sample Value"},
+    // paper_bgcolor: "rgb(208, 244, 253)",
+    // yaxis: {title:"UTO label"},
+    height: 0.6,
     margin: {
-        l: 150,
-        r: 100,
-        t: 100,
-        b: 25
-    }
+        l: 100
+    },
+    font: {family: "Lucida Handwriting"}
     };
 
 // Render the plot to the div tag with id "bar"
     Plotly.newPlot("bar", bar_data, layout);
-}
+
+    
+};
+
+function createBubble(data) {
+
+    console.log("Data Used for Bubble Chart");
+    console.log(data);
+    console.log(data.id);
+    var myId = data.id;
+    console.log(data.sample_values);
+    console.log(data.otu_ids);
+    console.log(data.otu_labels);
+
+    var trace1 = {
+        x: data.otu_ids,
+        y: data.sample_values,
+        text: data.otu_labels,
+        paper_bgcolor: "rgb(208, 244, 253)",
+        mode: 'markers',
+        marker: {
+          size: data.sample_values,
+          color: data.otu_ids
+        }
+      };
+      
+      var data = [trace1];
+
+      myHeight = Math.max(data.otu_ids)*1.2;
+      console.log(myHeight);
+
+      var layout = {
+        title: ("Test Subject ID: " + myId),
+        xaxis: {title:"Sample Value"},
+        showlegend: false,
+        height: myHeight,
+        width: 1140,
+        font: {family: "Lucida Handwriting"}    
+      };
+      
+      Plotly.newPlot("bubble", data, layout);
+};
+
+function createGauge(data) {
+    
+    var myFreq = data.wfreq;
+    console.log(myFreq);
+
+    var data = [
+        {
+            type: "indicator",
+            mode: "gauge+number",
+            value: myFreq,
+            title: { text: "Washing Frequency"},
+            // delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+            gauge: {
+              axis: { range: [null, 10], tickwidth: 1, tickcolor: "darkblue" },
+              bar: { color: "#023b55" },
+            //   bgcolor: "rgb(208, 244, 253)",
+              borderwidth: 2,
+              bordercolor: "gray",
+              steps: [
+                // color: rgb(67, 94, 73);
+                { range: [0,1], color: "rgb(0, 100, 0)"},
+                { range: [1,2], color: "rgb(20, 125, 16)" },
+                { range: [2,3], color: "rgb(40, 150, 32)" },
+                { range: [3,4], color: "rgb(60, 175, 48)" },
+                { range: [4,5], color: "rgb(80, 190, 64)" },
+                { range: [5,6], color: "rgb(100, 200, 90)" },
+                { range: [6,7], color: "rgb(125, 210, 120)" },
+                { range: [7,8], color: "rgb(150, 220, 150)" },
+                { range: [8,9], color: "rgb(175, 230, 175)" },
+                { range: [9,10], color: "rgb(208, 238, 207)" }
+              ],
+            //   threshold: {
+            //     line: { color: "green", width: 4 },
+            //     thickness: 0.75,
+            //     value: 10
+            //   }
+            }
+          }
+ 
+        // {   
+        //     values: [1,2,3,4,5,6,7,8,9,10],
+        //     domain: { x: [0, 1], y: [0, 10] },
+        //     value: myFreq,
+        //     steps: [
+        //         { range: [0, 250], color: "cyan" },
+        //         { range: [250, 400], color: "royalblue" }
+        //       ],
+        //     title: { text: "Washing Frequency" },
+        //     type: "indicator",
+        //     mode: "gauge+number"
+        // }
+    ];
+    
+    var layout = { 
+            width: 455, 
+            height: 300, 
+            margin: { t: 0, b: 0 } ,
+            font: {family: "Lucida Handwriting"}
+        };
+
+    Plotly.newPlot('gauge', data, layout);
+};
 
 // get_set.on("change", function() {optionChanged()});
 
